676335b1c35f1796eb6e887011199629b205f243
Go Takahana <tagahana508@gmail.com>
2021-12-25 21:35:01 +0900

    remove ToNative annotation

--- usecaseImpl/onboarding/src/commonMain/kotlin/tech/takahana/iconwallpaper/usecase/onboarding/WelcomeUseCaseImpl.kt
+++ usecaseImpl/onboarding/src/commonMain/kotlin/tech/takahana/iconwallpaper/usecase/onboarding/WelcomeUseCaseImpl.kt
@@ -1,9 +1,7 @@
 package tech.takahana.iconwallpaper.usecase.onboarding
 
-import com.futuremind.koru.ToNativeClass
 import tech.takahana.iconwallpaper.shared.coroutines.flow.MutableSharedFlowReplayable
 
-@ToNativeClass(name = "WelcomeUseCaseImplIos")
 class WelcomeUseCaseImpl : WelcomeUseCase {
 
     override val onFinishOnBoardingFlow = MutableSharedFlowReplayable<Unit>()
