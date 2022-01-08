7ab7744fe69fee93e9709638d886a1baa7beb882
Go Takahana <tagahana508@gmail.com>
2022-01-08 21:05:40 +0900

    Add propertyAsInt

--- buildSrc/src/main/java/tech/takahana/iconwallpaper/gradle/ProjectExt.kt
+++ buildSrc/src/main/java/tech/takahana/iconwallpaper/gradle/ProjectExt.kt
@@ -5,3 +5,7 @@ import org.gradle.api.Project
 fun Project.propertyAsBoolean(propertyName: String): Boolean {
     return findProperty(propertyName).toString().toBoolean()
 }
+
+fun Project.propertyAsInt(propertyName: String): Int {
+    return findProperty(propertyName).toString().toInt()
+}
