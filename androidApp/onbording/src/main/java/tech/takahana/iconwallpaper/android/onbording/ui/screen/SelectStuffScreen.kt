eefd02698cf18aa8ba13aa0b17347cd63b3e048f
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-01-10 15:09:10 +0900

    [change] Change icon to a shape
    アイコンを図形に変更

--- androidApp/onbording/src/main/java/tech/takahana/iconwallpaper/android/onbording/ui/screen/SelectStuffScreen.kt
+++ androidApp/onbording/src/main/java/tech/takahana/iconwallpaper/android/onbording/ui/screen/SelectStuffScreen.kt
@@ -1,6 +1,6 @@
 package tech.takahana.iconwallpaper.android.onbording.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,21 +35,12 @@ fun SelectStuffScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Announcements(message = stringResource(R.string.step1_seclect_stuff))
-                Column(
+                ItemGrid(columnNum = 5)
+                Box(
                     modifier = Modifier
-                        .padding(20.dp)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Bottom,
+                        .padding(20.dp),
+                    contentAlignment = Alignment.Center
                 ) {
-                    ItemGrid(
-                        listOf(
-                            painterResource(id = R.drawable.stuff_1),
-                            painterResource(id = R.drawable.stuff_2),
-                            painterResource(id = R.drawable.stuff_3),
-                            painterResource(id = R.drawable.stuff_4)
-                        )
-                    )
                     BottomButton(
                         onClick = { /*TODO*/ },
                         backgroundColor = Color.LightGray,
