7f0774d3f753038b138df43ac3b89fbfb9ee4c62
Go Takahana <tagahana508@gmail.com>
2022-02-06 22:53:03 +0900

    fix SelectImageAssetsScreen

--- androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/SelectImageAssetsScreen.kt
+++ androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/SelectImageAssetsScreen.kt
@@ -1,33 +1,87 @@
 package tech.takahana.iconwallpaper.android.home.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush.Companion.verticalGradient
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import tech.takahana.iconwallpaper.android.core.ui.components.RoundButton
+import tech.takahana.iconwallpaper.android.core.ui.theme.Gray100
+import tech.takahana.iconwallpaper.android.core.ui.theme.IconWallPaperTheme
 import tech.takahana.iconwallpaper.android.home.R
-import tech.takahana.iconwallpaper.android.home.ui.components.ItemGrid
+import tech.takahana.iconwallpaper.android.home.ui.components.ImageAssetItemGrid
 import tech.takahana.iconwallpaper.android.home.ui.components.StepAnnouncement
+import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetName
+import tech.takahana.iconwallpaper.shared.domain.domainobject.LocalImageAsset
 
 @Composable
-fun SelectImageAssetsScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+fun SelectImageAssetsScreen(
+    modifier: Modifier = Modifier
+) {
+    ConstraintLayout(
+        modifier = modifier.fillMaxSize()
     ) {
-        StepAnnouncement(message = stringResource(R.string.home_step1_seclect_stuff))
-        ItemGrid(columnNum = 5)
+        val (
+            stepAnnouncement,
+            itemGrid,
+            gradient,
+            button
+        ) = createRefs()
+
+        StepAnnouncement(
+            modifier = Modifier.constrainAs(stepAnnouncement) {
+                top.linkTo(parent.top)
+            },
+            message = stringResource(R.string.home_step1_seclect_image_assets)
+        )
+        ImageAssetItemGrid(
+            modifier = Modifier.constrainAs(itemGrid) {
+                top.linkTo(stepAnnouncement.bottom)
+                bottom.linkTo(button.top)
+                height = Dimension.fillToConstraints
+            },
+            items = (1..100).map {
+                LocalImageAsset(
+                    name = AssetName("cat"),
+                    resId = R.drawable.cat
+                )
+            }
+        )
         Box(
             modifier = Modifier
-                .padding(20.dp),
+                .constrainAs(gradient) {
+                    bottom.linkTo(button.top)
+                }
+                .background(
+                    verticalGradient(
+                        0.0f to Gray100.copy(alpha = 0f),
+                        0.2f to Gray100.copy(alpha = 0.5f),
+                        1f to Gray100
+                    )
+                )
+                .fillMaxWidth()
+                .height(48.dp)
+        )
+        Box(
+            modifier = Modifier
+                .constrainAs(button) {
+                    bottom.linkTo(parent.bottom)
+                }
+                .background(Gray100)
+                .padding(horizontal = 16.dp, vertical = 24.dp),
             contentAlignment = Alignment.Center
         ) {
             RoundButton(
@@ -42,5 +96,9 @@ fun SelectImageAssetsScreen() {
 @Preview(showSystemUi = true)
 @Composable
 private fun PreviewSelectStuffScreen() {
-    SelectImageAssetsScreen()
+    IconWallPaperTheme {
+        Surface {
+            SelectImageAssetsScreen()
+        }
+    }
 }
