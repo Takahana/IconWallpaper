39688ecc6154f19750d09f86159072d696593347
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-02-12 12:35:58 +0900

    [refactor] Formatting with the introduction of ktlint
    ktlintの導入によるフォーマット

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,12 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
         classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
     }
 }
 
 allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
     repositories {
         google()
         mavenCentral()
--- uilogic/welcome/build.gradle.kts
+++ uilogic/welcome/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     kotlin("native.cocoapods")
     kotlin("kapt")
     id("com.android.library")
-
 }
 
 version = "1.0"
@@ -12,7 +11,7 @@ kotlin {
     android()
     iosX64()
     iosArm64()
-    //iosSimulatorArm64() sure all ios dependencies support this target
+    // iosSimulatorArm64() sure all ios dependencies support this target
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -22,7 +21,7 @@ kotlin {
             baseName = "welcome"
         }
     }
-    
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -50,22 +49,22 @@ kotlin {
         }
         val iosX64Main by getting
         val iosArm64Main by getting
-        //val iosSimulatorArm64Main by getting
+        // val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            //iosSimulatorArm64Main.dependsOn(this)
+            // iosSimulatorArm64Main.dependsOn(this)
             kotlin.srcDir("${buildDir.absolutePath}/generated/source/kaptKotlin/")
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-        //val iosSimulatorArm64Test by getting
+        // val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            //iosSimulatorArm64Test.dependsOn(this)
+            // iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
