d3b1b1f15ef55e285b54f86992e7726e0149df97
Go Takahana <tagahana508@gmail.com>
2022-02-19 17:15:12 +0900

    delete HomeUseCase

--- uilogicImpl/home/src/commonMain/kotlin/tech/takahana/iconwallpapaer/uilogic/home/HomeUiLogicImpl.kt
+++ uilogicImpl/home/src/commonMain/kotlin/tech/takahana/iconwallpapaer/uilogic/home/HomeUiLogicImpl.kt
@@ -2,16 +2,14 @@ package tech.takahana.iconwallpapaer.uilogic.home
 
 import kotlinx.coroutines.flow.SharedFlow
 import tech.takahana.iconwallpaper.uilogic.home.HomeUiLogic
-import tech.takahana.iconwallpaper.usecase.home.HomeUseCase
 
 class HomeUiLogicImpl(
-    private val useCase: HomeUseCase
 ) : HomeUiLogic {
 
     override val finishedHomeEffect: SharedFlow<Unit>
-        get() = useCase.onFinishHomeFlow
+        get() = TODO()
 
     override suspend fun onClickedFinishButton() {
-        useCase.finishHome()
+        TODO()
     }
 }
