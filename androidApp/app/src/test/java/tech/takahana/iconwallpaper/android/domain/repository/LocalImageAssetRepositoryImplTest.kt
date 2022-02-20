a3f58aacedb36b67366ef1de113cb3bc4ae62117
Go Takahana <tagahana508@gmail.com>
2022-02-20 23:10:23 +0900

    Add LocalImageAssetRepositoryImplTest

---
+++ androidApp/app/src/test/java/tech/takahana/iconwallpaper/android/domain/repository/LocalImageAssetRepositoryImplTest.kt
@@ -0,0 +1,35 @@
+package tech.takahana.iconwallpaper.android.domain.repository
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual
+import org.junit.Before
+import org.junit.Test
+import tech.takahana.iconwallpaper.android.home.R
+import tech.takahana.iconwallpaper.shared.assets.LocalImageAsset
+import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetId
+import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetName
+
+class LocalImageAssetRepositoryImplTest {
+
+    private lateinit var repository: LocalImageAssetRepositoryImpl
+
+    @Before
+    fun setUp() {
+        repository = LocalImageAssetRepositoryImpl()
+    }
+
+    @Test
+    fun getAll() {
+        val expected = (1..100).map { num ->
+            LocalImageAsset(
+                id = AssetId("cat_$num"),
+                name = AssetName("cat"),
+                resId = R.drawable.cat
+            )
+        }
+
+        val actual = repository.getAll()
+
+        assertThat(actual, IsEqual(expected))
+    }
+}
