32d755da3227ae1be0aee53b20559d31737b1d78
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-03-06 11:34:32 +0900

    [update] Change image size and offset
    イメージのサイズとオフセットを変更

--- androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/components/ImagePattern.kt
+++ androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/components/ImagePattern.kt
@@ -5,11 +5,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toPixelMap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import tech.takahana.iconwallpaper.android.core.ui.theme.IconWallPaperTheme
 import tech.takahana.iconwallpaper.android.home.R
 import tech.takahana.iconwallpaper.android.home.ui.type.PatternType
@@ -19,38 +19,38 @@ fun ImagePattern(patternType: PatternType) {
     val image = ImageBitmap.imageResource(
         id = R.drawable.cat
     )
-    when (patternType) {
-        PatternType.SMALL -> {
-            Canvas(Modifier.fillMaxSize()) {
+    Canvas(Modifier.fillMaxSize()) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        when (patternType) {
+            PatternType.SMALL -> {
                 drawImage(
                     image = image,
+                    dstSize = IntSize(canvasWidth.toInt(), canvasHeight.toInt())
                 )
             }
-        }
-        PatternType.MEDIUM -> {
-            Canvas(Modifier.fillMaxSize()) {
-                val canvasSize = size
-                val canvasWidth = size.width
-                val canvasHeight = size.height
-                drawImage(
-                    image = ImageBitmap(
-                        width = canvasWidth.toInt() - 2,
-                        height = canvasHeight.toInt() - 2
-                    ).also {
-                        it.readPixels(
-                            width = canvasWidth.toInt() - 1,
-                            height = canvasHeight.toInt() - 1,
-                            buffer = image.toPixelMap().buffer
+            PatternType.MEDIUM -> {
+                for (i in 0 until 3) {
+                    if (i == 0) {
+                        drawImage(
+                            image = image,
+                            dstSize = IntSize(
+                                (canvasWidth / 3).toInt(),
+                                (canvasHeight / 3).toInt()
+                            ),
+                            dstOffset = IntOffset(x = 0, y = 0)
                         )
+                        continue
                     }
-                )
-                drawImage(
-                    image = image,
-                    topLeft = Offset(x = canvasWidth / 3F, y = 0F),
-                )
+                    drawImage(
+                        image = image,
+                        dstSize = IntSize((canvasWidth / 3).toInt(), (canvasHeight / 3).toInt()),
+                        dstOffset = IntOffset(x = (canvasWidth / 3 * i).toInt(), y = 0)
+                    )
+                }
             }
-        }
-        PatternType.LARGE -> {
+            PatternType.LARGE -> {
+            }
         }
     }
 }
