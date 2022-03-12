ec171d3b12bd84c4ee560b25b947e653b4ac7502
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-03-12 12:46:36 +0900

    [update] Update drawing of medium size patterns
    中サイズのパターンの描画をアップデート

--- androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/components/ImagePattern.kt
+++ androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/components/ImagePattern.kt
@@ -2,9 +2,12 @@ package tech.takahana.iconwallpaper.android.home.ui.components
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,6 +22,7 @@ fun ImagePattern(patternType: PatternType) {
     val image = ImageBitmap.imageResource(
         id = R.drawable.cat
     )
+    val backGroundColor = MaterialTheme.colors.background
     Canvas(Modifier.fillMaxSize()) {
         val canvasWidth = size.width
         val canvasHeight = size.height
@@ -30,24 +34,86 @@ fun ImagePattern(patternType: PatternType) {
                 )
             }
             PatternType.MEDIUM -> {
-                for (i in 0 until 3) {
-                    if (i == 0) {
-                        drawImage(
-                            image = image,
-                            dstSize = IntSize(
-                                (canvasWidth / 3).toInt(),
-                                (canvasHeight / 3).toInt()
-                            ),
-                            dstOffset = IntOffset(x = 0, y = 0)
-                        )
-                        continue
-                    }
-                    drawImage(
-                        image = image,
-                        dstSize = IntSize((canvasWidth / 3).toInt(), (canvasHeight / 3).toInt()),
-                        dstOffset = IntOffset(x = (canvasWidth / 3 * i).toInt(), y = 0)
+                val dstSize = IntSize((canvasWidth / 3).toInt(), (canvasHeight / 3).toInt())
+                val coverSize = Size((canvasWidth / 3 / 2), (canvasHeight / 3))
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(x = 0, y = 0)
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(x = (canvasWidth / 3 * 1).toInt(), y = 0)
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(x = (canvasWidth / 3 * 2).toInt(), y = 0)
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(
+                        x = (canvasWidth / 3 * -1 / 2).toInt(),
+                        y = (canvasHeight / 3).toInt()
                     )
-                }
+                )
+                drawRect(
+                    color = backGroundColor,
+                    topLeft = Offset(x = canvasWidth / 3 * -1 / 2, y = canvasHeight / 3F),
+                    size = coverSize
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(
+                        x = (canvasWidth / 3 * 1 / 2).toInt(),
+                        y = (canvasHeight / 3).toInt()
+                    )
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(
+                        x = (canvasWidth / 3 * 3 / 2).toInt(),
+                        y = (canvasHeight / 3).toInt()
+                    )
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(
+                        x = (canvasWidth / 3 * 5 / 2).toInt(),
+                        y = (canvasHeight / 3).toInt()
+                    )
+                )
+                drawRect(
+                    color = backGroundColor,
+                    topLeft = Offset(x = canvasWidth / 3 * 6 / 2, y = canvasHeight / 3F),
+                    size = coverSize
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(x = 0, y = (canvasHeight / 3 * 2).toInt())
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(
+                        x = (canvasWidth / 3 * 1).toInt(),
+                        y = (canvasHeight / 3 * 2).toInt()
+                    )
+                )
+                drawImage(
+                    image = image,
+                    dstSize = dstSize,
+                    dstOffset = IntOffset(
+                        x = (canvasWidth / 3 * 2).toInt(),
+                        y = (canvasHeight / 3 * 2).toInt()
+                    )
+                )
             }
             PatternType.LARGE -> {
             }
