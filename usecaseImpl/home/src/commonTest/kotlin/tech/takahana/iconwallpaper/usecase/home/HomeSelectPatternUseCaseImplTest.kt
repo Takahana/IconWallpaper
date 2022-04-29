595f461ac8edf0327e085113cae32c425482053e
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-04-29 20:51:18 +0900

    [fix] Fixed test
    テストの修正

--- usecaseImpl/home/src/commonTest/kotlin/tech/takahana/iconwallpaper/usecase/home/HomeSelectPatternUseCaseImplTest.kt
+++ usecaseImpl/home/src/commonTest/kotlin/tech/takahana/iconwallpaper/usecase/home/HomeSelectPatternUseCaseImplTest.kt
@@ -7,13 +7,18 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import tech.takahana.iconwallpaper.repository.asset.SelectImageAssetRepository
 import tech.takahana.iconwallpaper.repository.asset.SelectPatternTypeRepository
+import tech.takahana.iconwallpaper.shared.assets.LocalImageAsset
+import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetId
+import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetName
 import tech.takahana.iconwallpaper.shared.domain.domainobject.PatternType
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
 class HomeSelectPatternUseCaseImplTest {
@@ -32,9 +37,14 @@ class HomeSelectPatternUseCaseImplTest {
     @Test
     fun selectPattern() = runTest {
         val mockPatternType: PatternType = mockk()
-        every { mockSelectImageAssetRepository.selectedImageAssetFlow } returns flowOf(null)
+        val dummyLocalImageAsset = LocalImageAsset(
+            id = AssetId.requireGet("assetId1"),
+            name = AssetName("assetName1")
+        )
+        every { mockSelectImageAssetRepository.selectedImageAssetFlow } returns flowOf(
+            dummyLocalImageAsset
+        )
         every { mockSelectPatternTypeRepository.selectedPatternTypeFlow } returns flowOf(PatternType.SMALL)
-        coEvery { mockSelectImageAssetRepository.setSelectedImageAsset(any()) } returns Unit
         coEvery { mockSelectPatternTypeRepository.setSelectedPatternType(any()) } returns Unit
         val useCase = HomeSelectPatternUseCaseImpl(
             mockSelectPatternTypeRepository,
@@ -48,5 +58,33 @@ class HomeSelectPatternUseCaseImplTest {
                 mockPatternType
             )
         }
+    }
+
+    @Test
+    fun wrapFlow_inUseCaseModel() = runTest {
+        val dummyLocalImageAsset = LocalImageAsset(
+            id = AssetId.requireGet("assetId1"),
+            name = AssetName("assetName1")
+        )
+        every { mockSelectPatternTypeRepository.selectedPatternTypeFlow } returns flowOf(PatternType.SMALL)
+        every { mockSelectImageAssetRepository.selectedImageAssetFlow } returns flowOf(
+            dummyLocalImageAsset
+        )
+
+        val useCase = HomeSelectPatternUseCaseImpl(
+            mockSelectPatternTypeRepository,
+            mockSelectImageAssetRepository
+        )
+        val expectedPatternType = SelectedPatternUseCaseModel(PatternType.SMALL)
+
+        val expectedImageAsset = ImageAssetUseCaseModel.HasAsset(dummyLocalImageAsset, true)
+
+        val actualPatternType = useCase.selectedPatternFlow.first()
+
+        val actualImageAsset = useCase.selectedImageAssetFlow.first()
+
+        assertEquals(expectedPatternType, actualPatternType)
+
+        assertEquals(expectedImageAsset, actualImageAsset)
     }
 }
