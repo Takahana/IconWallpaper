ebce83cb2d55a16a9cf056cdab32e245b7ff3f5d
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-08-27 15:18:35 +0900

    [add] Added a screen to preview the cropped icon
    切り取ったアイコンをプレビューする画面を追加

---
+++ androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/HomePreviewIconContent.kt
@@ -0,0 +1,66 @@
+package tech.takahana.iconwallpaper.android.home.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import tech.takahana.iconwallpaper.android.core.ui.components.RoundButton
+import tech.takahana.iconwallpaper.android.home.R
+
+@Composable
+fun HomePreviewIconContent(
+    homeNavController: NavHostController
+) {
+    val image = ImageBitmap.imageResource(
+        id = R.drawable.cat
+    )
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(bitmap = image, contentDescription = null)
+        Text(text = "この画像でよろしいですか？")
+        Row(
+            modifier = Modifier.padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            RoundButton(
+                modifier = Modifier.fillMaxWidth(0.5F),
+                onClick = { /*TODO  前の画面に戻る*/ },
+                backgroundColor = Color.LightGray,
+                text = "いいえ",
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            RoundButton(
+                modifier = Modifier,
+                onClick = { /*TODO  パターン選択画面に遷移*/ },
+                backgroundColor = MaterialTheme.colors.primary,
+                text = "はい",
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PreviewHomePreviewIconContent() {
+    HomePreviewIconContent(homeNavController = rememberNavController())
+}
