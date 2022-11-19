ead04500f4d70ac4be377facab0bcdef5d5a3a7f
Go Takahana <tagahana508@gmail.com>
2022-11-19 15:30:57 +0900

    format code

--- repositoryFake/src/commonMain/kotlin/tech/takahana/iconwallpaper/repository/asset/FakeSelectBackgroundColorRepository.kt
+++ repositoryFake/src/commonMain/kotlin/tech/takahana/iconwallpaper/repository/asset/FakeSelectBackgroundColorRepository.kt
@@ -3,7 +3,6 @@ package tech.takahana.iconwallpaper.repository.asset
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import tech.takahana.iconwallpaper.repository.asset.SelectBackgroundColorRepository
 import tech.takahana.iconwallpaper.shared.domain.domainobject.ColorType
 
 class FakeSelectBackgroundColorRepository : SelectBackgroundColorRepository {
--- repositoryFake/src/commonMain/kotlin/tech/takahana/iconwallpaper/repository/asset/FakeSelectPatternTypeRepository.kt
+++ repositoryFake/src/commonMain/kotlin/tech/takahana/iconwallpaper/repository/asset/FakeSelectPatternTypeRepository.kt
@@ -3,7 +3,6 @@ package tech.takahana.iconwallpaper.repository.asset
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import tech.takahana.iconwallpaper.repository.asset.SelectPatternTypeRepository
 import tech.takahana.iconwallpaper.shared.domain.domainobject.PatternType
 
 class FakeSelectPatternTypeRepository : SelectPatternTypeRepository {
