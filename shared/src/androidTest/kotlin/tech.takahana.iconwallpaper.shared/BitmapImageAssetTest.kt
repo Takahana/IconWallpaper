6f365723f1a3580550b95d26ff13044dce314493
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2022-12-09 00:14:15 +0900

    [fix] Fixing BitmapImageAssetTest instances, etc.
    BitmapImageAssetTestのインスタンスらへんの修正

--- shared/src/androidTest/kotlin/tech.takahana.iconwallpaper.shared/BitmapImageAssetTest.kt
+++ shared/src/androidTest/kotlin/tech.takahana.iconwallpaper.shared/BitmapImageAssetTest.kt
@@ -3,17 +3,25 @@ package tech.takahana.iconwallpaper.shared
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
 import tech.takahana.iconwallpaper.shared.assets.BitmapImageAsset
 import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetId
 import tech.takahana.iconwallpaper.shared.domain.domainobject.AssetName
 
 class BitmapImageAssetTest {
-    @Test
-    fun recycleBitmap() {
-        val bitmapImageAsset = BitmapImageAsset(
+
+    lateinit var bitmapImageAsset: BitmapImageAsset
+
+    @Before
+    fun before() {
+        bitmapImageAsset = BitmapImageAsset(
             id = AssetId("assetId"), name = AssetName("assetName"), bitmap = mockk()
         )
+    }
+
+    @Test
+    fun recycleBitmap() {
         val bitmap = bitmapImageAsset.bitmap
         every { bitmap.recycle() } returns Unit
 
