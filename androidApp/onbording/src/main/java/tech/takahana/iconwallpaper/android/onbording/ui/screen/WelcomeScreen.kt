f56b8f3c01d130a13aea15daeb8b60e8caa59e95
Go Takahana <tagahana508@gmail.com>
2023-02-19 16:28:03 +0900

    replace welcome image

--- androidApp/onbording/src/main/java/tech/takahana/iconwallpaper/android/onbording/ui/screen/WelcomeScreen.kt
+++ androidApp/onbording/src/main/java/tech/takahana/iconwallpaper/android/onbording/ui/screen/WelcomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
@@ -13,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -35,12 +37,12 @@ fun WelcomeScreen(
         verticalArrangement = Arrangement.Bottom,
     ) {
         Image(
-            painter = painterResource(id = R.drawable.welcome_dog),
+            painter = painterResource(id = R.drawable.welcome),
             contentDescription = stringResource(R.string.welcome_dog),
             modifier = Modifier
                 .weight(1f)
-                .fillMaxSize(),
-            contentScale = ContentScale.Crop
+                .fillMaxWidth(),
+            contentScale = ContentScale.Fit
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
