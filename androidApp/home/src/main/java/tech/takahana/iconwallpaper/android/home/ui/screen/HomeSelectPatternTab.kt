50e18e9da87aa99daea0540b89253fa51a04d9b3
Go Takahana <tagahana508@gmail.com>
2023-02-19 14:16:02 +0900

    format

--- androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/HomeSelectPatternTab.kt
+++ androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/HomeSelectPatternTab.kt
@@ -2,7 +2,6 @@ package tech.takahana.iconwallpaper.android.home.ui.screen
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
