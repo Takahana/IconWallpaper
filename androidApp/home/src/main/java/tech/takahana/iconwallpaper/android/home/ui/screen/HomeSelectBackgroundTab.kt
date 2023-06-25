7cf6518fd1158cf918fe1ce5af38f5e6b8f05580
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2023-06-25 15:04:05 +0900

    Revert "[update] Simplified logic"
    This reverts commit 8d65760fb16bbda412d68b596b93822ae2f900bc.

--- androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/HomeSelectBackgroundTab.kt
+++ androidApp/home/src/main/java/tech/takahana/iconwallpaper/android/home/ui/screen/HomeSelectBackgroundTab.kt
@@ -24,28 +24,31 @@ fun HomeSelectBackgroundTab(
     val onClick = { color: ColorType ->
         selectBackgroundColorUiLogic.onClickedBackgroundColor(color)
     }
+    val isChecked = { colorType: ColorType ->
+        colorType == backgroundColor
+    }
     Column {
         Row(
             modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center
         ) {
             ColorButton(
-                color = ColorType.Black, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Black, isChecked = isChecked(ColorType.Black), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.White, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.White, isChecked = isChecked(ColorType.White), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.Red, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Red, isChecked = isChecked(ColorType.Red), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.Blue, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Blue, isChecked = isChecked(ColorType.Blue), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.Green, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Green, isChecked = isChecked(ColorType.Green), onClick = onClick
             )
         }
         Spacer(modifier = Modifier.padding(8.dp))
@@ -53,20 +56,21 @@ fun HomeSelectBackgroundTab(
             modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center
         ) {
             ColorButton(
-                color = ColorType.Purple, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Purple, isChecked = isChecked(ColorType.Purple), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.Yellow, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Yellow, isChecked = isChecked(ColorType.Yellow), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.Beige, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Beige, isChecked = isChecked(ColorType.Beige), onClick = onClick
             )
             Spacer(modifier = Modifier.padding(12.dp))
             ColorButton(
-                color = ColorType.Cyan, backgroundColor = backgroundColor, onClick = onClick
+                color = ColorType.Cyan, isChecked = isChecked(ColorType.Cyan), onClick = onClick
             )
+            Spacer(modifier = Modifier.padding(12.dp))
             // TODO: その他は後ほど実装
             // OutlinedButton(
             //   modifier = Modifier
